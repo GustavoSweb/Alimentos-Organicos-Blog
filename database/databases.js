943d183182fc9bfffdf8ae6fe29b9162358a2d96
gustavo <gustavodasilvama10@gmail.com>
2023-06-27 23:13:19 -0300

    vaii ag

--- database/databases.js
+++ database/databases.js
@@ -1,6 +1,6 @@
 const sequelize= require("sequelize")
 
-const connection = new sequelize ("postgresql://GustavoSweb:ODWTzcsl97eh@ep-lively-rain-503674.us-east-2.aws.neon.tech/neondb", {
+const connection = new sequelize ("postgresql://GustavoSweb:ODWTzcsl97eh@ep-lively-rain-503674.us-east-2.aws.neon.tech/neondb?sslmode=require", {
     dialectModule: require('pg')
 })
 
